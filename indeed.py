088288abe98e1844408fe818e627804f4874bac3
sarori <salo3678@gmail.com>
2020-05-10 01:12:34 +0200

    Indeed Scrapping

--- indeed.py
+++ indeed.py
@@ -19,10 +19,28 @@ def extract_indeed_pages():
     max_page = pages[-1]
     return max_page
 
+def extract_job(html):
+    title = html.find("h2", {"class": "title"}).find("a")["title"]
+    company = html.find("span", {"class": "company"})
+    company_anchor = company.find("a")
+    if company_anchor is not None:
+        company = str(company_anchor.string)
+    else:
+        company = str(company.string)
+    company = company.strip()
+    location = html.find("div", {"class" : "recJobLoc"})["data-rc-loc"]
+    job_id = html["data-jk"]
+    return {'title': title, 'company': company, 'location': location, "link": f"https://www.indeed.fr/emplois?as_and=python&as_phr&as_any&as_not&as_ttl&as_cmp&jt=all&st&as_src&salary&radius=25&l=Nanterre%20(92)&fromage=any&sort&psf=advsrch&from=advancedsearch&advn=4923530667854545&vjk={job_id}"}
+
 
 def extract_indeed_jobs(last_page):
     jobs = []
     for page in range(last_page):
+        print(f"Scrapping page {page}")
         result = requests.get(f"{URL}&start={page*LIMIT}")
-        print(result.status_code)
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class" : "jobsearch-SerpJobCard"})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
     return jobs
