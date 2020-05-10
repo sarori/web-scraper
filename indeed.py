34bb05b6839c90eb1473ea1494305d309e80f3e0
sarori <salo3678@gmail.com>
2020-05-11 01:06:52 +0200

    Stack Overflow Scrapping

--- indeed.py
+++ indeed.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 LIMIT = 50
 URL = f"https://www.indeed.fr/emplois?as_and=python&as_phr=&as_any=&as_not=&as_ttl=&as_cmp=&jt=all&st=&as_src=&salary=&radius=25&l=Nanterre+(92)&fromage=any&limit={LIMIT}&sort=&psf=advsrch&from=advancedsearch"
 
-def extract_indeed_pages():
+def get_last_page():
     result = requests.get(URL)
 
     soup = BeautifulSoup(result.text, "html.parser")
@@ -33,14 +33,18 @@ def extract_job(html):
     return {'title': title, 'company': company, 'location': location, "link": f"https://www.indeed.fr/emplois?as_and=python&as_phr&as_any&as_not&as_ttl&as_cmp&jt=all&st&as_src&salary&radius=25&l=Nanterre%20(92)&fromage=any&sort&psf=advsrch&from=advancedsearch&advn=4923530667854545&vjk={job_id}"}
 
 
-def extract_indeed_jobs(last_page):
+def extract_jobs(last_page):
     jobs = []
     for page in range(last_page):
-        print(f"Scrapping page {page}")
         result = requests.get(f"{URL}&start={page*LIMIT}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class" : "jobsearch-SerpJobCard"})
         for result in results:
             job = extract_job(result)
             jobs.append(job)
+    return jobs
+
+def get_jobs():
+    last_pages = get_last_page()
+    jobs = extract_jobs(last_pages)
     return jobs
